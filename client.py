d0edd3fb8873d3d1afdf91e949fc3ed262b935fd
Catalin Popescu <catalinp86@gmail.com>
2018-03-18 13:22:58 -0400

    started dev of commandProcessing clientSide

--- client.py
+++ client.py
@@ -40,21 +40,59 @@ def displayMainMenu():
             continue
     # return userChoice #should never happen
 
+def processCommand(userChoice):
+    command = ''
+    if userChoice is 1:
+        command = findCommand()
+    elif userChoice is 2:
+        command = 'user wants to add a customer'
+    elif userChoice is 3:
+        command = 'user wants to delete a customer'
+    elif userChoice is 4:
+        command = 'user wants to update a customer\'s age'
+    elif userChoice is 5:
+        command = 'user wants to update a customer\'s address'
+    elif userChoice is 6:
+        command = 'user wants to update a customer\'s phone number'
+    elif userChoice is 7:
+        command = 'user wants to print a report with all the entries'
+    elif userChoice is 8:
+        command = 'user wants to exit'
+    print(command)
+    return
+        
+def findCommand():
+    command = ''
+    clearScreen()
+    print()
+    print(' * * * * * * * * * * * * * * * *')
+    print(' * Find customer :             *')
+    print(' * * * * * * * * * * * * * * * *')
+    name = ''
+    while name == '':
+        name = input(' * Name: ')
+        if name is '':
+            print(' * You must provide a name, please try again')
+    command = 'find|' + name
+    return command 
+
 # Create a socket object
 s = socket.socket()         
 
 # Define the port on which you want to connect
 port = 9999             
 
-userChoic = displayMainMenu()
-print(userChoic)
-userChoice = 'find|john'
+userChoice = displayMainMenu()
+print(processCommand(userChoice))
+command = 'find|john'
 
 
 # connect to the server on local computer
 s.connect(('127.0.0.1', port))
 
-s.send(userChoice.encode('utf-8'))
+#send command
+s.send(command.encode('utf-8'))
+
 returned = s.recv(1024)
 returned = returned.decode('utf-8')
 print(returned)
